c283bdcd4c5f50c5346df3c15ef0c42375fa6c5b
rohits1301998 <rohits130198@gmail.com>
2017-03-04 15:01:02 +0530

    MainFinal.java
    modified for n bits addition

--- Main.java
+++ Main.java
@@ -1,27 +1,27 @@
-package com.company;
+
 
 import java.util.Scanner;
 
-public class Main
+public class NewClass1
 {
     public static void main(String args[])
     {
         Scanner Sc=new Scanner(System.in);
         int carry=0,sum;
-        System.out.println("Enter two 4-Bit Binary numbers:");
-        int n1=Sc.nextInt();
-        int n2=Sc.nextInt();
-        int a[]=new int[5];
-        for(int i=a.length-1;i>=0;i--)
+        System.out.println("Enter two Binary numbers:");
+        String s1=Sc.next();
+        String s2=Sc.next();
+        int n1=Integer.parseInt(s1);
+        int n2=Integer.parseInt(s2);
+        int a[]=new int[s1.length()+1];
+        for(int i=a.length-1;i>=1;i--)
         {
           int x=n1%10;
           int y=n2%10;
           n1=n1/10;
           n2=n2/10;
           sum=x+y+carry;
-          if(sum==1){
-              a[i]=sum;carry=0;}
-          else if(sum==2)
+          if(sum==2)
           {
               a[i]=0;
               carry=1;
@@ -31,10 +31,12 @@ public class Main
               a[i]=1;
               carry=1;
           }
-          else {
-              a[i] = x + y + carry;carry=0;
+          else{
+              a[i]=sum;
+              carry=0;
           }
         }
+        a[0]=carry;
         for(int i=0;i<a.length;i++)
             System.out.print(a[i]);
 
